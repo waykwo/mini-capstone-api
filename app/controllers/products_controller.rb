5e685d66702a2fa8bd3482c5cd036368adb1251a
waykwo <72991933+waykwo@users.noreply.github.com>
2024-09-01 18:46:15 -0400

    Add update and destroy methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,4 +20,22 @@ class ProductsController < ApplicationController
     @product.save
     render template: "products/show"
   end
+
+  def update
+    p params
+    @product = Product.find_by(id: params[:id])
+    @product.name = params[:name]
+    @product.price = params[:price]
+    @product.image_url = params[:image_url]
+    @product.description = params[:description]
+    @product.save
+    render template: "products/show"
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+    render template: "products/show"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get "/products" => "products#index"
   get "/products/:id" => "products#show"
   post "/products" => "products#create"
+  patch "/products/:id" => "products#update"
+  delete "/products/:id" => "products#destroy"
 
   # Defines the root path route ("/")
   # root "posts#index"
