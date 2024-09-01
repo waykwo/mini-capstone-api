a3087fdc7bcb529da5bcce36584f2a845e75aba6
waykwo <72991933+waykwo@users.noreply.github.com>
2024-09-01 17:48:03 -0400

    Add hard-coded create method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,21 @@
 class ProductsController < ApplicationController
-  def all
+  def index
     @products = Product.all
     render template: "products/index"
-
-    # render json: {message: "test"}
   end
 
-  def one
-    @product = Product.find_by(id: params["id"])
+  def show
+    @product = Product.find_by(id: params[:id])
+    render template: "products/show"
+  end
+
+  def create
+    @product = Product.new(
+      name: "glass",
+      price: 12,
+      description: "great for holding liquids but holds some solids too"
+    )
+    @product.save
     render template: "products/show"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get "/products" => "products#all"
-  get "/one_product/:id" => "products#one"
+  get "/products" => "products#index"
+  get "/products/:id" => "products#show"
+  post "/products" => "products#create"
 
   # Defines the root path route ("/")
   # root "posts#index"
