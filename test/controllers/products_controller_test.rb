f53f7f3a7e44e48f459d9aa1708f50fd77b91966
waykwo <72991933+waykwo@users.noreply.github.com>
2024-09-04 19:46:15 -0400

    Add cors gem and configure for Peter's front end

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -8,4 +8,36 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal Product.count, data.length
   end
+
+  test "show" do
+    get "/products/#{Product.first.id}.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal ["id", "name", "price", "image_url", "description"], data.keys
+  end
+
+  test "create" do
+    assert_difference "Product.count", 1 do
+      post "/products.json", params: { name: "product_test", price: 0, image_url: "http://example.com", description: "describes product" }
+      assert_response 200
+    end
+  end
+
+  test "update" do
+    product = Product.first
+    patch "/products/#{product.id}.json", params: { name: "Updated name" }
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal "Updated name", data["name"]
+  end
+
+  test "destroy" do
+    assert_difference "Product.count", -1 do
+      delete "/products/#{Product.first.id}.json"
+      assert_response 200
+    end
+  end
+
 end
