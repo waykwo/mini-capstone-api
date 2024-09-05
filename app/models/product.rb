156a44bf7fc75bd85cf05afd9b3ae2649f98a8a3
waykwo <72991933+waykwo@users.noreply.github.com>
2024-09-04 20:19:49 -0400

    Add discounted, tax, and total methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,15 @@
 class Product < ApplicationRecord
+  def is_discounted?
+    if price <= 10
+      return true
+    end
+  end
 
+  def tax
+    return (price * 0.09).round(2)
+  end
+
+  def total
+    return (price + tax).round(2)
+  end
 end
